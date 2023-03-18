9c1f358a267de135c77ba8315b9ceacf72ca5581
Chiara <chiarafdono@gmail.com>
2023-03-18 19:49:04 -0300

    Agregados ejercicios de recursión sobre números

--- Practica2.hs
+++ Practica2.hs
@@ -137,23 +137,34 @@ elMinimo (x:xs) = if x < elMinimo xs
 -- Defina las siguientes funciones utilizando recursión sobre números enteros, salvo que se indique lo contrario:
 -- 1. Dado un número n se devuelve la multiplicación de este número y todos sus anteriores hasta llegar a 0. Si n es 0 devuelve 1. 
 -- La función es parcial si n es negativo.
---factorial :: Int -> Int
---factorial n = n * factorial (n-1) 
+factorial :: Int -> Int -- PRECOND.: n >= 0
+factorial 0 = 1
+factorial n = n * factorial (n-1)
 
 -- 2. Dado un número n devuelve una lista cuyos elementos sean los números comprendidos entre n y 1 (incluidos). Si el número es 
 -- inferior a 1, devuelve la lista vacía.
--- cuentaRegresiva :: Int -> [Int]
+cuentaRegresiva :: Int -> [Int]
+cuentaRegresiva 0 = []
+cuentaRegresiva n = n : cuentaRegresiva (n-1)
 
 -- 3. Dado un número n y un elemento e devuelve una lista en la que el elemento e repite n veces.
--- repetir :: Int -> a -> [a]
+repetir :: Int -> a -> [a]
+repetir 0 x = []
+repetir n x = x : repetir (n-1) x
 
 -- 4. Dados un número n y una lista xs, devuelve una lista con los n primeros elementos de xs. Si la lista es vacía, devuelve una 
 -- lista vacía.
--- losPrimeros :: Int -> [a] -> [a]
+losPrimeros :: Int -> [a] -> [a]
+losPrimeros 0 _      = []
+losPrimeros n []     = []
+losPrimeros n (x:xs) = x : losPrimeros (n-1) xs
 
 -- 5. Dados un número n y una lista xs, devuelve una lista sin los primeros n elementos de lista recibida. Si n es cero, devuelve la 
 -- lista completa.
--- sinLosPrimeros :: Int -> [a] -> [a]
+sinLosPrimeros :: Int -> [a] -> [a]
+sinLosPrimeros _ []     = []
+sinLosPrimeros 0 xs     = xs
+sinLosPrimeros n (x:xs) = sinLosPrimeros (n-1) xs
 
 -- 3. Registros
 -- 1. Definir el tipo de dato Persona, como un nombre y la edad de la persona. Realizar las siguientes funciones:
